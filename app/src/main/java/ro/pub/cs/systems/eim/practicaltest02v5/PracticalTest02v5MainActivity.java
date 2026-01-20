fedc89ef079af6b49e0217c73a2d872154d63863
SpottedIonut <120397620+SpottedIonut@users.noreply.github.com>
2026-01-20 18:38:10 +0200

    init

---
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest02v5/PracticalTest02v5MainActivity.java
@@ -0,0 +1,18 @@
+package ro.pub.cs.systems.eim.practicaltest02v5;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class PracticalTest02v5MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+    }
+}
